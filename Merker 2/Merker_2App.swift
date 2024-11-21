050d74c1f96c7f69ac990b8b6fe9f0467b84909f
Siryl <siryl95@hotmail.com>
2024-11-22 00:16:26 +0100

    Initial Commit

---
+++ Merker 2/Merker_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Merker_2App.swift
+//  Merker 2
+//
+//  Created by Siryl Souissi on 22.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Merker_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Merker 2Tests/Merker_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Merker_2Tests.swift
+//  Merker 2Tests
+//
+//  Created by Siryl Souissi on 22.11.2024.
+//
+
+import Testing
+@testable import Merker_2
+
+struct Merker_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
